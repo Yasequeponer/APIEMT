54f6b16a0c9e6061e31ee7414962c6530f07311f
Yasequeponer <diegosarango.garcia@gmail.com>
2026-05-11 20:06:18 +0200

    Initial commit

---
+++ src/main/java/com/example/apiemt/ApiemtApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiemt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiemtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiemtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiemt/ApiemtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiemt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiemtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
