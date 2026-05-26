5da59eb8cfdc0483504c32f4d98cc66290ab0b68
Yasequeponer <diegosarango.garcia@gmail.com>
2026-05-26 02:20:18 +0200

    Corrección de URL "application.properties" e incorporacion de exclusor para pruebas Corrección de implementación "WebClient"

--- src/main/java/com/example/apiemt/config/WebClientConfig.java
+++ src/main/java/com/example/apiemt/config/WebClientConfig.java
@@ -3,15 +3,16 @@ package com.example.apiemt.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClient {
+public class WebClientConfig {
 
     @Value("${api.externa.URL}")
     private String url;
 
     @Bean
-    public org.springframework.web.reactive.function.client.WebClient webClient(){
-        return org.springframework.web.reactive.function.client.WebClient.builder().baseUrl(url).build();
+    public WebClient webClient(){
+        return WebClient.builder().baseUrl(url).build();
     }
 }
