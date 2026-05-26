331f5a3d4b7ada5167caa0e9426633994c222e5b
Yasequeponer <diegosarango.garcia@gmail.com>
2026-05-27 01:05:48 +0200

    Agregación de distintos JsonProperty para corregir el mappeo

--- src/main/java/com/example/apiemt/dto/ParadaResponse.java
+++ src/main/java/com/example/apiemt/dto/ParadaResponse.java
@@ -4,6 +4,7 @@ package com.example.apiemt.dto;
 * */
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -11,22 +12,31 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true) //Para ignorar cualquier propiedad descconocida en el JSON
 @Data
 public class ParadaResponse {
-    private List<Data> data;
+    private List<Datos> data;
 
-    public static class Data{
+    @JsonIgnoreProperties(ignoreUnknown = true) //Para ignorar cualquier propiedad descconocida en el JSON
+    @Data
+    public static class Datos{
+        @JsonProperty("Arrive")
         private List<Arrive> arrive;
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true) //Para ignorar cualquier propiedad descconocida en el JSON
+    @Data
     public static class Arrive{
         private String line; //Numero de linea de bus
         private String stop; //Numero de parada
         private String destination; //Direccion del bus
         private int bus; //Numero de bus
         private Geometry geometry; //Objeto JSON que contiene las coordenadas de la ubicación de la parada
+        @JsonProperty("estimateArrive")
         private int estimateArrive; //Tiempo en segundos para la llegada del bus
-        private int DistanceBus; //Metros que separan el bus de la parada
+        @JsonProperty("DistanceBus")
+        private int distanceBus; //Metros que separan el bus de la parada
     }
 
+    @JsonIgnoreProperties(ignoreUnknown = true) //Para ignorar cualquier propiedad descconocida en el JSON
+    @Data
     public static class Geometry{
         private List<Float> coordinates; //Coordenadas de la ubicación de la parada
     }
